f22e35296475ea800c0a8a35bedf7350bade7389
Siyanda-Mzam <dot.netntoz@gmail.com>
2016-07-20 21:02:28 +0200

    Fixed the data structure's data, and managed to capture links between. Need to populate the rooms with links and start moving-in ants.

--- main.c
+++ main.c
@@ -8,8 +8,26 @@ typedef struct 		s_list_of_rooms
 	int				x;
 	int				y;
 	char			*name;
+	char			*links[10000];
+	int				is_start;
+	int				is_end;
+	int				is_non;
 	struct s_list_of_rooms	**next;
 }					t_room;
+
+t_room		*dictionary(char *key, t_room **value)
+{
+	unsigned int		i;
+
+	i = 0;
+	while ((*value))
+	{
+		if (!ft_strcmp(value[i]->name, key))
+			return value[i];
+		i++;
+	}
+	return (0);
+}
 
 t_room	*new_room(char *name)
 {
@@ -26,7 +44,7 @@ t_room	*new_room(char *name)
 			free(new_room);
 			return (NULL);
 		}
-		new_room->name = strdup(splitted_specs[0]);
+		new_room->name = ft_strdup(splitted_specs[0]);
 
 		new_room->x = ft_atoi(splitted_specs[1]);
 		new_room->y = ft_atoi(splitted_specs[2]);
@@ -44,29 +62,47 @@ t_room	*new_room(char *name)
 
 t_room	*get_rooms_and_coords(char ***lines, t_room **rooms)
 {
-	if (strchr((*lines)[0], '-') != NULL)
+	if (lines && *lines && ***lines && ft_strchr((*lines)[0], '-'))
 	{
-		printf("Yes: \n"/*, (*lines)[0]*/);
-		(*lines)++;
+		int		i;
+		char	**links;
+		t_room	*tmp;
+
+		i = -1;
+		while (lines && *lines && (*lines)[++i] && ***lines)
+		{
+			if (strchr((*lines)[i], '#'))
+				(*lines)++;
+			links = ft_strsplit((*lines)[i], '-');
+			tmp = dictionary(links[0], rooms);
+			tmp->links[i] = ft_strdup(links[1]);
+			ft_memcpy((*rooms)->links[0], tmp->links[i], ft_strlen(tmp->links[i])); 
+			printf("Room: %s\tLink %d is [%s, %s]\n", (*rooms)->name, i + 1, links[0], (*rooms)->links[0]);
+		}
 	}
 	else if (!strcmp((*lines)[0], "##start"))
 	{
 		(*lines)++;
 		*rooms = new_room((*lines)[0]);
-		printf("START: %d, %d\n", (*rooms)->x, (*rooms)->y);
+		(*rooms)->is_start = 1;
+		(*rooms)->is_end = 0;
 	}
 	else if (!strcmp((*lines)[0], "##end"))
 	{
 		(*lines)++;
 		*rooms = new_room((*lines)[0]);
-		printf("END: %d, %d\n", (*rooms)->x, (*rooms)->y);
+		(*rooms)->is_start = 0;
+		(*rooms)->is_end = 1;
 	}
 	else if (!ft_strcmp((*lines)[0], "#comment") || !ft_strcmp((*lines)[0], "##comment") || !ft_strcmp(ft_strsub((*lines)[0], 0, 6), "#anoth"))
 	{
 		(*lines)++;
 		*rooms = new_room((*lines)[0]);
+		(*rooms)->is_start = 0;
+		(*rooms)->is_end = 0;
+		(*rooms)->is_non = 1;
 	}
-	else
+	else if (lines && *lines && **lines && ***lines != EOF)
 	{
 		*rooms = new_room((*lines)[0]);
 		//(*lines)++;
@@ -74,67 +110,55 @@ t_room	*get_rooms_and_coords(char ***lines, t_room **rooms)
 	return (*rooms);
 }
 
-int		len(char ***s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (((*s) + i))
-		i++;
-	return (i);
-}
-
-void	find_links(char ***lines, t_room **rooms)
-{
-
-}
-
 int		main(int argc, char **argv)
 {
 	mode_t		mode;
 	int 		fd;
 	int 		i;
-	int			ant_amt;
+	unsigned int			ant_amt;
 	t_room		*rooms;
 	t_room		*room_lst[100000];
-	int			nbl;
-	char		*room_name;
-	char 		s[1000000];
+	char 		stream[1000000];
+	t_room		**rlst;
 	char		**lines;
-	char		**alias;
+	char		**line_alias;
 
 	i = -1;
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fd = open ("map.txt", O_RDONLY | O_CREAT, mode);
-	read(fd, s, 1000000000);
-	s[strlen(s) + 1] = 0;
-	lines = ft_strsplit(s, '\n');
-	alias = lines;
-	/*Map read test */
-	printf("Map:\n");
+	read(fd, stream, 1000000000);
+	stream[ft_strlen(stream) + 1] = 0;
+	lines = ft_strsplit(stream, '\n');
+	line_alias = lines;
+	rlst = room_lst;
 	while (lines[++i])
 	{
 		ft_memcpy(lines[i], (lines[i] = ft_strtrim(lines[i])), ft_strlen(lines[i]));
-		//printf("%s\n", lines[i++]);
 	}
-	//nbl = len(&lines);
-	/* Map read end */
-	//printf("%d\n", nbl);
-	/* Ant read test */
-	//printf("Before cut down: %s\n", lines[0]);
 	ant_amt = ft_atoi(lines[0]);
-	//lines++;
-	//printf("After cut down: %s\n", lines[0]);
-	/* Ant read end */
 	i = -1;
-	while (!ft_strchr(*lines, '-') && lines && *(lines++))
-		room_lst[++i] =  get_rooms_and_coords(&lines, &rooms);
-//	while (lines && ft_strchr(*lines, '-'))
-	//	find_links(&lines, &rooms);
-	room_lst[i] = 0;
+	while (!strchr(*lines, '-') && lines && *(lines++))
+		rlst[++i] =  get_rooms_and_coords(&lines, &rooms);
+	rlst[i] = 0;
 	i = -1;
-	while (room_lst[++i])
-		printf("Room name: %s,\tx: %d,\ty: %d\n", (room_lst[i])->name, (room_lst[i])->x, (room_lst[i])->y);
-	free(alias);
+	while (rlst[++i])
+	{
+		if ((rlst[i])->is_start)
+			printf("This is the start: %s\n", rlst[i]->name);
+		if ((rlst[i])->is_end)
+			printf("This is the end: %s\n", rlst[i]->name);
+		else
+		{
+			printf("Rest of the rooms: %s\n", rlst[i]->name);
+		}
+	}
+	i = -1;
+	while (rlst[++i])
+	{
+		//printf("Room name: %s,\tx: %d,\ty: %d\n", rlst[i]->name, rlst[i]->x, rlst[i]->y);
+		printf("Room: %s is linked to %s\n", rlst[i]->name, rlst[i]->links[i]);
+	}
+	printf("dictionary: %s\n", (dictionary("6", rlst))->name);
+	free(line_alias);
 	return (0);
 }
