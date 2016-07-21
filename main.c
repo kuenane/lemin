1f957ff5c5bae5fe254f5942dc03ee594395b3d7
Siyanda-Mzam <dot.netntoz@gmail.com>
2016-07-21 07:40:30 +0200

    Now its time to play the game in code.

--- main.c
+++ main.c
@@ -15,15 +15,15 @@ typedef struct 		s_list_of_rooms
 	struct s_list_of_rooms	**next;
 }					t_room;
 
-t_room		*dictionary(char *key, t_room **value)
+t_room		*dictionary(char *key, t_room ***value)
 {
 	unsigned int		i;
 
 	i = 0;
-	while ((*value))
+	while ((**value))
 	{
-		if (!ft_strcmp(value[i]->name, key))
-			return value[i];
+		if (!ft_strcmp((*value)[i]->name, key))
+			return ((*value)[i]);
 		i++;
 	}
 	return (0);
@@ -45,9 +45,9 @@ t_room	*new_room(char *name)
 			return (NULL);
 		}
 		new_room->name = ft_strdup(splitted_specs[0]);
-
-		new_room->x = ft_atoi(splitted_specs[1]);
-		new_room->y = ft_atoi(splitted_specs[2]);
+		if (splitted_specs && splitted_specs[1])
+		{new_room->x = ft_atoi(splitted_specs[1]);
+		new_room->y = ft_atoi(splitted_specs[2]);}
 	}
 	else
 	{
@@ -55,7 +55,8 @@ t_room	*new_room(char *name)
 		new_room->x = 0;
 		new_room->y = 0;
 	}
-	new_room->next = NULL;
+	if (new_room)
+		new_room->next = NULL;
 	return (new_room);
 }
 
@@ -64,21 +65,7 @@ t_room	*get_rooms_and_coords(char ***lines, t_room **rooms)
 {
 	if (lines && *lines && ***lines && ft_strchr((*lines)[0], '-'))
 	{
-		int		i;
-		char	**links;
-		t_room	*tmp;
-
-		i = -1;
-		while (lines && *lines && (*lines)[++i] && ***lines)
-		{
-			if (strchr((*lines)[i], '#'))
-				(*lines)++;
-			links = ft_strsplit((*lines)[i], '-');
-			tmp = dictionary(links[0], rooms);
-			tmp->links[i] = ft_strdup(links[1]);
-			ft_memcpy((*rooms)->links[0], tmp->links[i], ft_strlen(tmp->links[i])); 
-			printf("Room: %s\tLink %d is [%s, %s]\n", (*rooms)->name, i + 1, links[0], (*rooms)->links[0]);
-		}
+		return (*rooms)++;
 	}
 	else if (!strcmp((*lines)[0], "##start"))
 	{
@@ -105,12 +92,31 @@ t_room	*get_rooms_and_coords(char ***lines, t_room **rooms)
 	else if (lines && *lines && **lines && ***lines != EOF)
 	{
 		*rooms = new_room((*lines)[0]);
-		//(*lines)++;
 	}
 	return (*rooms);
 }
 
-int		main(int argc, char **argv)
+int		len(char **s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int		rlen(t_room **s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int		main(void)
 {
 	mode_t		mode;
 	int 		fd;
@@ -132,14 +138,46 @@ int		main(int argc, char **argv)
 	line_alias = lines;
 	rlst = room_lst;
 	while (lines[++i])
-	{
 		ft_memcpy(lines[i], (lines[i] = ft_strtrim(lines[i])), ft_strlen(lines[i]));
-	}
 	ant_amt = ft_atoi(lines[0]);
 	i = -1;
-	while (!strchr(*lines, '-') && lines && *(lines++))
+	while (lines && !strchr(*lines, '-') && *(lines++))
 		rlst[++i] =  get_rooms_and_coords(&lines, &rooms);
-	rlst[i] = 0;
+	while (lines && (strchr(*lines, '-')) && *(lines++))
+	{
+
+		int		i;
+		int		k;
+		char	**links;
+		t_room	*tmp;
+
+		i = -1;
+		k = 0;
+		while (lines && *lines && **lines &&  (strchr(*lines, '-')))
+		{
+			++i;
+			while (strchr(lines[i], '#'))
+			{
+				lines++;
+				i = 0;
+			}
+			links = ft_strsplit(lines[i], '-');
+			tmp = dictionary(links[0], &rlst);
+			tmp->links[len(tmp->links)] = ft_strdup(links[1]);
+			//printf("Room: %s\tis linked to room [%s]\n", dictionary(links[0], &rlst)->name, dictionary(links[0], &rlst)->links[0]);
+			tmp = dictionary(links[1], &rlst);
+			tmp->links[len(tmp->links)] = ft_strdup(links[0]);
+			//printf("Room: %s\tis linked to room [%s]\n\n", dictionary(links[1], &rlst)->name, dictionary(links[1], &rlst)->links[0]);
+			lines++;
+			i = 0;
+		}
+	//	if (lines && !(strchr(*lines, '-')))
+		//	lines++;
+		
+			printf("seemingly\n");
+	}
+	printf("seemingly %d\n", i);
+	rlst[rlen(rlst) + 1] = 0;
 	i = -1;
 	while (rlst[++i])
 	{
@@ -147,18 +185,19 @@ int		main(int argc, char **argv)
 			printf("This is the start: %s\n", rlst[i]->name);
 		if ((rlst[i])->is_end)
 			printf("This is the end: %s\n", rlst[i]->name);
-		else
-		{
-			printf("Rest of the rooms: %s\n", rlst[i]->name);
-		}
 	}
 	i = -1;
+	int b;
 	while (rlst[++i])
 	{
+		b = i;
 		//printf("Room name: %s,\tx: %d,\ty: %d\n", rlst[i]->name, rlst[i]->x, rlst[i]->y);
-		printf("Room: %s is linked to %s\n", rlst[i]->name, rlst[i]->links[i]);
+		printf("Room: %s is linked to:\n", rlst[b]->name);
+		while (rlst && rlst[b] && rlst[b]->links[i])
+			printf("%s\n", rlst[b]->links[i++]);
+		i = b;
 	}
-	printf("dictionary: %s\n", (dictionary("6", rlst))->name);
+	printf("dictionary: %s\n", (dictionary("6", &rlst))->name);
 	free(line_alias);
 	return (0);
 }
